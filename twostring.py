d13df46e6c32d05f664001774e3cc9374df7c530
aayushiinfra <aayushiyadav221@gmail.com>
2023-08-11 18:59:23 +0530

    Basic functionalities accepts strings only

--- twostring.py
+++ twostring.py
@@ -1,7 +1,7 @@
 # Code for checking if two strings are anagrams
 
-s1 = input ("\tEnter string1\t\n")
-s2 = input ("\tEnter string2\t\n")
+s1 = input ("\tEnter string 1\t\n")
+s2 = input ("\tEnter string 2\t\n")
 
 def verify( s1, s2):
     if sorted(s1)== sorted(s2):
@@ -9,4 +9,7 @@ def verify( s1, s2):
     else:
         print("\t\n Yay !!! Strings are not anagrams \n")
 
-verify (s1, s2)
+if  s1.isalpha() and  s2.isalpha():
+    verify(s1,s2)
+else:
+    print("\t\nEnter only alphabetical characters\n")
